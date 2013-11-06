bd41fd1939def937317d74420ad3695479d10aca
nguyenti <tn.xc.lax@gmail.com>
2013-11-05 21:12:24 -0600

    working test1 iterativemerge

--- src/edu/grinnell/csc207/sorting/IterativeMergeSorter.java
+++ src/edu/grinnell/csc207/sorting/IterativeMergeSorter.java
@@ -42,25 +42,52 @@ public class IterativeMergeSorter<T> extends SorterBridge<T> {
 	    T[] temp1 = (T[]) new Object[size];
 	    T[] temp2 = (T[]) new Object[size];
 	    T[] output = (T[]) new Object[size * 2];
-	    while (numMerge < vals.length) {
-		System.arraycopy(vals, numMerge - size, temp1, 0, size);
-		System.arraycopy(vals, numMerge, temp2, 0, size);
+	    if (size * 2 <= vals.length) {
+		while (numMerge < vals.length) {
+		    System.arraycopy(vals, numMerge - size, temp1, 0, size);
+		    System.arraycopy(vals, numMerge, temp2, 0, size);
+		    output = Utils.merge(order, temp1, temp2);
+		    // FILL IN!
+		    for (int i = 0; i < output.length; i++) {
+			vals[i + numMerge - size] = output[i];
+		    } // for
+		    numMerge += size * 2;
+		} // while
+		numMerge -= size;
+		int diff = vals.length - numMerge;
+		if (diff > 0) {
+		    int lastSize = size / 2;
+		    int lastleftover = numMerge + lastSize;
+		    int leftoverOLeftover = vals.length - lastleftover;
+		    output = (T[]) new Object[diff];
+		    temp1 = (T[]) new Object[lastSize];
+		    temp2 = (T[]) new Object[leftoverOLeftover];
+		    if (diff > lastSize) {
+			System.arraycopy(vals, numMerge, temp1, 0, lastSize);
+		    } else {
+			System.arraycopy(vals, numMerge, temp1, 0, diff);
+		    }
+		    if (leftoverOLeftover != 0) {
+			System.arraycopy(vals, lastleftover, temp2, 0,
+				leftoverOLeftover);
+		    }
+		    output = Utils.merge(order, temp1, temp2);
+		    // FILL IN!
+		    for (int i = 0; i < diff; i++) {
+			vals[numMerge + i] = output[i];
+		    }
+		}
+	    } else {
+		output = (T[]) new Object[vals.length];
+		temp1 = (T[]) new Object[size];
+		temp2 = (T[]) new Object[vals.length - size];
+		System.arraycopy(vals, 0, temp1, 0, size);
+		System.arraycopy(vals, size, temp2, 0,
+			vals.length - size);
 		output = Utils.merge(order, temp1, temp2);
 		// FILL IN!
-		for (int i = 0; i < output.length; i++) {
-		    vals[i + numMerge - size] = output[i];
-		} // for
-		numMerge += size * 2;
-	    } // while
-	    numMerge -= size;
-	    int diff = vals.length - numMerge;
-	    if (diff > 1) {
-		System.arraycopy(vals, numMerge, temp1, 0, diff);
-		temp2 = (T[]) new Object[0];
-		output = Utils.merge(order, temp1, temp2);
-		// FILL IN!
-		for (int i = 0; i < diff; i++) {
-		    vals[numMerge + i] = output[i];
+		for (int i = 0; i < vals.length; i++) {
+		    vals[i] = output[i];
 		}
 	    }
 	    // The merged subarrays are now twice as large
